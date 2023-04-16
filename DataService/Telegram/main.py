0e322c059e71af48621d7a78d7e513d62d892be1
elmiado <sayad.mahdi75@gmail.com>
2023-04-16 18:34:44 +0330

    hardcode the chatid of a test channel

--- DataService/Telegram/main.py
+++ DataService/Telegram/main.py
@@ -7,9 +7,7 @@ async def get_channel_messages():
     bot = telegram.Bot(token='6093000872:AAEq5nIqK4YisVNCjBxNP5trD4gM3Wb4GYg')
 
     # Get the channel ID
-    channel_name = 'AbbasKiarostamiFans'
-    channel = await bot.get_chat(channel_name)
-    channel_id = channel.id
+    channel_id = '-1001704783944'
 
     # Get the latest messages from the channel
     messages = await bot.get_chat_messages(channel_id)
